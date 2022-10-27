3c29b628d18814900afb4da443f8cf979adc7cf4
Ahmad Fikri <90230134+fikriiashidqi@users.noreply.github.com>
2022-10-27 18:28:53 +0700

    Add files via upload

---
+++ Project 1 5220600020 & 5220600015/Projek1/Data.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projekan1
+{
+    public class JSONResponse
+    {
+        public List<Data> data { get; set; }
+        public Meta meta { get; set; }
+
+    }
+
+    public class Data
+    {
+        public float height { get; set; }
+        public DateTime time { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Meta
+    {
+        public string cost {  get; set; }
+        public string DailyQuota { get; set; }
+        public string datum { get; set; }
+        public DateTime end { get; set; }
+        public string lat { get; set; }
+        public string lng { get; set; }
+        public string requestCount { get; set; }
+        public DateTime start { get; set; }
+
+        public Station station { get; set; }
+    }
+
+    public class Station
+    {
+        public string distance { get; set; }
+        public string lat { get; set; }
+        public string lng { get; set; }
+        public string name { get; set; }
+        public string source { get; set; }
+
+    }
+}
